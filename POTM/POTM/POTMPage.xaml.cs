1527633c06d600788e1844fc31b454f46e79b2ad
Anders Larsen <anderslarsen@Anderss-MacBook-Pro-2.local>
2018-04-19 10:52:24 +0200

    XAML setup

---
+++ POTM/POTM/POTMPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace POTM
+{
+    public partial class POTMPage : ContentPage
+    {
+        public POTMPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
